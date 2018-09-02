14b21b8a7fa6d0344d91b49e67947850401fe009
xavierleonardo <gxavier.leonardo@gmail.com>
2018-09-02 20:31:55 -0300

    Restaurants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RestaurantsService } from './restaurants/restaurants.service';
 import { ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 
 
 @NgModule({
@@ -17,14 +19,15 @@ import { RestaurantsComponent } from './restaurants/restaurants.component'
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    RestaurantsComponent
+    RestaurantsComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Restaurant } from "./restaurant/restaurant.model";
+import { RestaurantsService } from "./restaurants.service";
 
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html"
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  restaurants: Restaurant[]
+
+  constructor(private restaurantsService: RestaurantsService) {}
 
   ngOnInit() {
+    this.restaurants = this.restaurantsService.restaurants()
   }
-
 }
