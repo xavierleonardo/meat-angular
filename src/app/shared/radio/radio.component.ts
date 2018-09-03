7966a11180bb9e869adc324fa31f38b70637fa8a
xavierleonardo <gxavier.leonardo@gmail.com>
2018-09-03 16:57:08 -0300

    Implementação do lógica de opções de pagamento

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { RadioOption } from './radio-option.model';
+import { Component, OnInit, Input, forwardRef } from "@angular/core";
+import { RadioOption } from "./radio-option.model";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
 @Component({
-  selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  selector: "mt-radio",
+  templateUrl: "./radio.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  @Input()
+  options: RadioOption;
 
-  @Input() options: RadioOption
+  value: any;
 
-  value: any
+  onChange: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   setValue(value: any) {
-    this.value = value
+    this.value = value;
+    this.onChange(this.value);
   }
 
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {}
+
+  setDisabledState?(isDisabled: boolean): void {}
 }
