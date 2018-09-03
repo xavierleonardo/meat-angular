7e404e69748bb3c007050467c02debf257f69ac0
xavierleonardo <gxavier.leonardo@gmail.com>
2018-09-03 18:33:13 -0300

    Configuração do envio da ordem para o back end

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,17 @@
+import { MEAT_API } from './../app.api';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from 'rxjs/Observable';
+import { Order, OrderItem } from './order.model';
+
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService) {}
+  constructor(private cartService: ShoppingCartService, private http: Http) {}
 
   itemsValue(): number {
     return this.cartService.total()
@@ -25,6 +31,19 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item)
+  }
+
+  clear() {
+    this.cartService.clear()
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    return this.http.post(`${MEAT_API}/orders`,
+                          JSON.stringify(order),
+                          new RequestOptions({headers: headers}))
+                    .map(response => response.json())
   }
 
 }
