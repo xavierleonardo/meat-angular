2edb577d013788b2af142165bb146701572a7e8f
xavierleonardo <gxavier.leonardo@gmail.com>
2018-09-06 16:53:56 -0300

    Lazy Loading (Feature Module) para o Sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
-import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { Routes } from "@angular/router";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
@@ -10,7 +9,7 @@ import { OrderComponent } from './order/order.component'
 
 export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
+  { path: "about", loadChildren: './about/about.module#AboutModule' },
   { path: "restaurants", component: RestaurantsComponent },
   { path: "restaurants/:id", component: RestaurantDetailComponent,
     children: [
